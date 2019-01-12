3c3029a37c565abd2fc534e42f4d3441ce01fa57
Ned <Ned612@Gmail.com>
2019-01-11 22:46:11 -0500

    Init

---
+++ Ranorex.XMLReader/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Ranorex.XMLReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string elementName = "activity";
+
+            XDocument xml = XDocument.Load("SampleReport.rxlog.data");
+            List<XElement> elements = xml.Descendants(elementName).ToList();
+            foreach (XElement element in elements)
+            {
+                Console.WriteLine(element.Value.Trim());
+            }
+
+            Console.Read();
+        }
+    }
+}
